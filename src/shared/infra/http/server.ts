798079eba846c7704543f95065344356d1a78c46
lucas vinicius <lucasviniciusaalves@gmail.com>
2022-01-18 10:23:09 -0300

    update: Refactory Code

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,31 @@
+import 'reflect-metadata';
+import log from '@config/logger';
+import express from 'express';
+
+import 'express-async-errors';
+import { AppError } from '@shared/errors/AppError';
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${err.message}`,
+    });
+  },
+);
+
+app.listen(PORT, () => {
+  log.info(`🚀 Server started on port ${PORT}`);
+});
